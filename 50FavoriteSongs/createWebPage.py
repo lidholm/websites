893b45a6b82c5091537348ac0c37a9e55dcece99
Andreas Lidholm <lidholm.andreas@gmail.com>
2015-09-29 00:21:31 +0200

    Started editing the index.html page and also changed to use link instead of embeded video

--- 50FavoriteSongs/createWebPage.py
+++ 50FavoriteSongs/createWebPage.py
@@ -21,29 +21,50 @@ class WebPageCreator():
             f.write(content)
            
     def makeWebPage(self):
+        
+        webPage = self.getStartPart()
+        for i in range(len(self.urls)):
+            #webPage += self.getEmbededVideoPart(self.urls[i].strip(), self.titles[i].strip())
+            webPage += self.getLink(self.urls[i].strip(), self.titles[i].strip())
+            
+            
+        webPage += self.getEndPart()
+        
+        self.savePage('allLinks.html', webPage)
+    
+    def getStartPart(self):
         start = """<html>
   <head>
     <title>50 Favorite Songs</title>
   </head>
   <body>
+  
 """
+        return start
+    
+    def getEndPart(self):
         end = """
   </body>
 </html>"""
-
+        return end
+    
+    def getEmbededVideoPart(self, url, title):
         linkAddress = 'https://www.youtube.com/watch?v=%s'
         thumbnail = '<img src="http://i1.ytimg.com/vi/%s/1.jpg" alt="%s" title="%s" />'
         
         imgLink = '    <a href="%s">%s</a>\n' % (linkAddress, thumbnail)
+        imgLink % (url, url, title, title )
         
+        return imgLink
+    
+    
+    def getLink(self, url, title):
+        linkAddress = 'https://www.youtube.com/watch?v=%s' % url
         
-        webPage = start
-        for i in range(len(self.urls)):
-            webPage += imgLink % (self.urls[i].strip(), self.urls[i].strip(), self.titles[i].strip(), self.titles[i].strip() )
-            
-        webPage += end
+        imgLink = '    <a href="%s">%s</a><br />\n' % (linkAddress, title)
         
-        self.savePage('allLinks.html', webPage)
+        return imgLink
+    
     
 if __name__ == "__main__":
     creator = WebPageCreator()
